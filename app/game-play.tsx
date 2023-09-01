d776d7bfc638c25c5cde128c42a75ec1ada83fc9
sohanemon <m.sohanemon@gmail.com>
2023-09-01 22:26:36 +0600

    feat: svg injection

--- app/game-play.tsx
+++ app/game-play.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import { ComponentProps } from 'react';
+import * as Tabs from '@radix-ui/react-tabs';
+import Img from '@sohanemon/next-image';
 import { cn } from '@sohanemon/utils';
 
 import { Text } from '@/components/ui/text';
 import { Images } from '@/components/images';
+
+import Globe from '../public/assets/images/globe.svg';
+import Plugin from '../public/assets/images/plugin.svg';
 
 interface GamePlayProps extends ComponentProps<'div'> {}
 
@@ -15,8 +22,38 @@ export default function GamePlay({ className, ...props }: GamePlayProps) {
         </Text>
         <Images.lotusSm className="absolute inset-x-0 -top-4 m-auto w-[175px]" />
         <Images.lotusXl className="fixed -top-16  right-0 m-auto w-[524px]" />
-        compt
+        <Tabs.Root>
+          <Tabs.List className="my-8 space-x-20">
+            {tabs.map((el) => (
+              <Tabs.Trigger
+                className="group inline-flex items-center gap-2"
+                value={el.label}
+              >
+                {/* {el.icon} */}
+                {/* <el.icon className="group-aria-selected:text-accent w-7 text-white" /> */}
+                <Text
+                  variant="lg/bold/default"
+                  className="group-aria-selected:text-accent"
+                >
+                  {el.label}
+                </Text>
+              </Tabs.Trigger>
+            ))}
+          </Tabs.List>
+          <Tabs.Content value=""></Tabs.Content>
+        </Tabs.Root>
       </div>
     </center>
   );
 }
+
+const tabs = [
+  {
+    label: 'Introduction to Huazang World',
+    icon: <Globe />,
+  },
+  {
+    label: 'Introduction to Fragment Gaming',
+    icon: <Plugin />,
+  },
+];
